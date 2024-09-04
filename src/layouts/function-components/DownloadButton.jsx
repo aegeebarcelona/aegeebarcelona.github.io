c8ec6301126f798aeabd7876b34a129adca419a9
Jaume Ros Alonso <jaume.ros.alo@gmail.com>
2024-09-04 22:33:44 +0200

    Fix calendar button not centered

--- src/layouts/function-components/DownloadButton.jsx
+++ src/layouts/function-components/DownloadButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DownloadButton = ({ file }) => {
   return (
     <div>
-      <a className="btn btn-outline-primary w-60" href={file} download="aegee-barceona-calendar.ics">Download calendar</a>
+      <a className="btn btn-outline-primary min-w-60" href={file} download="aegee-barceona-calendar.ics">Download calendar</a>
     </div>
   );
 }
